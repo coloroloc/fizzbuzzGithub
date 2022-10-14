6768090e19ae2cd64496e176a7e95457a0c2ef18
aeri m <89711861+coloroloc@users.noreply.github.com>
2022-10-14 17:27:42 -0500

    Update fizzbuzz.py
    fizzbuzz code is more efficient

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,15 +1,18 @@
+def fizzbuzz(listName):
+    
+    for x in listName:
+
+        if (x%3==0 and x%5==0):
+            print("fizzbuzz")
+
+        elif (x%3==0):
+            print("fizz")
+
+        elif (x%5==0):
+            print("buzz")
+
+        else:
+            print(x)
+
 numList = list(range(1, 101))
-
-for x in numList:
-
-    if (x%3==0 and x%5==0):
-        print("fizzbuzz")
-
-    elif (x%3==0):
-        print("fizz")
-
-    elif (x%5==0):
-        print("buzz")
-        
-    else:
-        print(x)
+fizzbuzz(numList)
